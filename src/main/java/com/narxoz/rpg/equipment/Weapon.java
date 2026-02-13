62af1d839bd0caf0bf83d09bf62d1372c5f121c5
Daniel Smakov <danieldiaz.smakov@gmail.com>
2026-02-13 23:36:40 +0500

    Replace Armor and Weapon interfaces with abstract classes

--- src/main/java/com/narxoz/rpg/equipment/Weapon.java
+++ src/main/java/com/narxoz/rpg/equipment/Weapon.java
@@ -1,11 +1,29 @@
 package com.narxoz.rpg.equipment;
 
-public interface Weapon {
-    String getName();
-    int getDamage();
-    WeaponType getWeaponType();
+public abstract class Weapon {
+    protected final String name;
+    protected final int damage;
+    protected final WeaponType weaponType;
 
-    default String getWeaponInfo() {
-        return getName() + " [" + getWeaponType() + "], DMG=" + getDamage();
+    protected Weapon(String name, int damage, WeaponType weaponType) {
+        this.name = name;
+        this.damage = damage;
+        this.weaponType = weaponType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public WeaponType getWeaponType() {
+        return weaponType;
+    }
+
+    public String getWeaponInfo() {
+        return name + " [" + weaponType + "], DMG=" + damage;
     }
 }
